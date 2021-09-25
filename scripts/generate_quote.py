16d66cd54f664cbe261f6ff3b19dab0795f50165
Vibhakar Solanki <solankivibhakar82@gmail.com>
2021-09-25 06:21:41 -0700

    Create generate_quote.py

---
+++ scripts/generate_quote.py
@@ -0,0 +1,30 @@
+import urllib.request
+import json
+
+def getResponse(url):
+    operUrl = urllib.request.urlopen(url)
+    if(operUrl.getcode()==200):
+        data = operUrl.read()
+        jsonData = json.loads(data)
+    else:
+        print("Error receiving data", operUrl.getcode())
+    return jsonData
+
+def write_to_readme(static_readme , quote):
+    lines  = open(static_readme , 'r').readlines()
+    lines +='\nquote of the day :\n'
+    lines += quote+'\n'
+    open('./README.md' , 'w').writelines(lines)
+
+def main():
+    url_eng = "https://api.quotable.io/random?tags=technology"
+    quote_response = getResponse(url_eng)
+    context = quote_response['content']
+    author = quote_response['author']
+    quote = context + " -" + author
+    # print(quote)
+    static_readme = './static_readme.md'
+    write_to_readme(static_readme , quote)
+
+if __name__ == '__main__':
+    main()
