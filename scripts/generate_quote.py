f115e94f6f7dbc80d380b3094ba42d188e269d98
Vibhakar Solanki <solankivibhakar82@gmail.com>
2021-09-25 06:34:20 -0700

    write quote to start

--- scripts/generate_quote.py
+++ scripts/generate_quote.py
@@ -10,18 +10,18 @@ def getResponse(url):
         print("Error receiving data", operUrl.getcode())
     return jsonData
 
-def write_to_readme(static_readme , quote):
-    lines  = open(static_readme , 'r').readlines()
-    lines +='\nquote of the day :\n'
-    lines += quote+'\n'
-    open('./README.md' , 'w').writelines(lines)
+def write_to_readme(static_readme , quote):    
+    with open(static_readme,'r') as f:
+        with open('./README.md','w') as f2: 
+            f2.write(quote)
+            f2.write(f.read())
 
 def main():
     url_eng = "https://api.quotable.io/random?tags=technology"
     quote_response = getResponse(url_eng)
     context = quote_response['content']
     author = quote_response['author']
-    quote = context + " -" + author
+    quote = ">" + context + " -" + author
     # print(quote)
     static_readme = './static_readme.md'
     write_to_readme(static_readme , quote)
