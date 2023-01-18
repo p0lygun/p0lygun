c847084113d1c0c38f958f82b2d931e896700589
p0lygun <solankivibhakar82@gmail.com>
2023-01-18 22:59:47 +0530

    feat: remove the need of static_readme

--- scripts/generate_quote.py
+++ scripts/generate_quote.py
@@ -1,30 +1,30 @@
 import urllib.request
 import json
 
-def getResponse(url):
-    operUrl = urllib.request.urlopen(url)
-    if(operUrl.getcode()==200):
-        data = operUrl.read()
-        jsonData = json.loads(data)
-    else:
-        print("Error receiving data", operUrl.getcode())
-    return jsonData
 
-def write_to_readme(static_readme , quote):    
-    with open(static_readme,'r') as f:
-        with open('./README.md','w') as f2: 
-            f2.write(quote)
-            f2.write(f.read())
+def get_response(url) -> dict | str:
+    url = urllib.request.urlopen(url)
+    if url.getcode() == 200:
+        return json.loads(url.read())
+    print("Error receiving data", url.getcode())
+    return ''
+
+
+def write_to_readme(quote):
+    with open('./README.md', 'r+') as file:
+        data = file.read().split("***", maxsplit=1)
+        file.seek(0)
+        file.write(quote.strip() + "\n***\n" + data[-1].strip())
+
+
 
 def main():
     url_eng = "https://api.quotable.io/random?tags=technology"
-    quote_response = getResponse(url_eng)
-    context = quote_response['content']
-    author = quote_response['author']
-    quote = ">" + context + " -" + author
+    quote_response = get_response(url_eng)
+    quote = ">" + quote_response['content'] + " -" + quote_response['author']
     # print(quote)
-    static_readme = './static_readme.md'
-    write_to_readme(static_readme , quote)
+    write_to_readme(quote)
+
 
 if __name__ == '__main__':
     main()
