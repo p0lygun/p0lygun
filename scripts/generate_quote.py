fef7e745285dbe1da1fd4ab49eeac4c2c8680157
MagneticNeedle <vibhakar.solanki@magnifi.ai>
2024-11-03 16:23:33 +0530

    fix(app): update quotes api url

--- scripts/generate_quote.py
+++ scripts/generate_quote.py
@@ -2,12 +2,12 @@ import urllib.request
 import json
 
 
-def get_response(url) -> dict | str:
+def get_response(url) -> dict:
     url = urllib.request.urlopen(url)
     if url.getcode() == 200:
         return json.loads(url.read())
     print("Error receiving data", url.getcode())
-    return ''
+    return dict()
 
 
 def write_to_readme(quote):
@@ -18,13 +18,18 @@ def write_to_readme(quote):
         file.truncate()
 
 
-
-def main():
-    url_eng = "https://api.quotable.io/random?tags=technology"
-    quote_response = get_response(url_eng)
-    quote = ">" + quote_response['content'] + " -" + quote_response['author']
+def main() -> None:
+    quote_response = get_response(
+        "https://quoteslate.vercel.app/api/quotes/random?tags=knowledge"
+    )
+    quote_parts = [
+        ">",
+        quote_response.get('quote', 'Sleep is the ultimate debugger'),
+        " -",
+        quote_response.get('author	', 'Vibhakar Solanki')
+    ]
     # print(quote)
-    write_to_readme(quote)
+    write_to_readme(''.join(quote_parts))
 
 
 if __name__ == '__main__':
